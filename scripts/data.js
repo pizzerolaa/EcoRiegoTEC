402059f3e1a7ebecc45b23b806855376cbf02721
Fernando Antonio López García <A01643685@tec.mx>
2023-11-09 14:24:47 -0600

    second-commit

---
+++ scripts/data.js
@@ -0,0 +1,9 @@
+document.getElementById("year_").innerHTML = new Date().getFullYear();
+
+function toggleMenu() {
+    var menu = document.querySelector('nav ul.menu-open');
+    menu.classList.toggle('show-menu');
+    menu.classList.add('active');
+}
+
+
